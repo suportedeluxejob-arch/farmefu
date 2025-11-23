df282e822ad563676e1c95ed56a4ac5ab4763a29
suportedeluxejob-arch <suporte.deluxejob@gmail.com>
2025-11-23 16:06:46 -0300

    fs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,10 +1,10 @@
 // postcss.config.mjs
-import tailwindcss from 'tailwindcss'
+import tailwindcss from '@tailwindcss/postcss'
 import autoprefixer from 'autoprefixer'
 
 export default {
   plugins: {
-    tailwindcss,
+    ...tailwindcss(),
     autoprefixer,
   },
 }
